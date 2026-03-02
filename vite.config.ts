abd3a3a1bba4fbb76438f0a130bf24be02f9f1b2
Angel K <angelkp1102@gmail.com>
2026-03-02 13:57:26 +0530

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Landing-page-Shri-Ashtalakshmi-Electicals-
-/',
+  base: '/Landing-page-Shri-Ashtalakshmi-Electicals-/',
   plugins: [react()],
 });
